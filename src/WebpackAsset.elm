a7498dc6d2ffda7927a2b915b1520fb201eafa8d
Jason O'Neil <jason.oneil@gmail.com>
2019-02-26 17:01:28 +0800

    Add documentation for Elm package

--- src/WebpackAsset.elm
+++ src/WebpackAsset.elm
@@ -1,10 +1,39 @@
-module WebpackAsset exposing (assetUrl)
+module WebpackAsset exposing (AssetUrl, assetUrl)
 
+{-| This library lets you reference webpack assets in your Elm views,
+and have them replaced with require() statements (and eventually, the URLs
+of the assets) at compile time. Designed to be used with the corresponding
+Babel plugin.
 
+@docs AssetUrl
+@docs assetUrl
+-}
+
+{-| An AssetUrl will hold the URL of your asset at runtime
+
+The value is provided by webpack, the same as `require('./logo.svg')` would be in JS.
+
+    view model =
+        div []
+            [ img [ src (assetUrl "./logo.svg") ] []
+            ]
+
+-}
 type alias AssetUrl =
     String
 
+{-| A function to get the URL of a webpack asset.
 
+The path you supply must be a literal string, not a variable or function that returns a string.
+
+It must also be relative to the root of your Elm project, rather than being relative to the current file.
+
+    view model =
+        div []
+            [ img [ src (assetUrl "./logo.svg") ] []
+            ]
+
+-}
 assetUrl : String -> AssetUrl
 assetUrl path =
     -- Webpack will replace this path with a URL Webpack at build time
